46a0dceb0a1de4dbacd58bd013734849a5d6f4e0
Alexey Karimurzayev <asxtream@gmail.com>
2022-10-02 17:52:52 +0600

    bubbles, done!!!

--- lib/bubbles_provider.dart
+++ lib/bubbles_provider.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_app_advanced_youtube/main.dart';
 
-class BubbleModelNotifier extends ChangeNotifier {
-  bool sizeUp = false;
+// ignore: must_be_immutable
+class RoundWithActionWidget extends StatelessWidget {
+  final Color color;
+  double size;
+  VoidCallback? stop;
+  bool removeBubble;
 
-  void resize({required bool sizeUp}) {
-    if (sizeUp) {
-      sizeUp = true;
-      notifyListeners();
+  RoundWithActionWidget({
+    required this.color,
+    required this.size,
+    required this.removeBubble,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final bubbleModel = context.watch<RoundModelEditorNotifier>();
+    if (bubbleModel.downsize) {
+      size = size * 0.98;
     }
+    if (size > 0 && size < 0.05.sw) {
+      size = 0;
+      removeBubble = true;
+    }
+    if (size >= 0.98.sw) {
+      size = 0;
+      removeBubble = true;
+    }
+
+    return GestureDetector(
+      onTap: () {
+        size = size * 1.2;
+        bubbleModel.editSize(resize: true);
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        height: size,
+        width: size,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: const BorderRadius.all(Radius.circular(200)),
+        ),
+        onEnd: () {
+          if (size == 0 && removeBubble) {
+            bubbleModel.editSize(resize: true);
+          }
+        },
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,37 +3,66 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
-import 'package:provider_app_advanced_youtube/balls.dart';
 import 'package:provider_app_advanced_youtube/bubbles_provider.dart';
 
 void main() {
-  runApp(const BallsApp());
+  runApp(NotMyBubbles());
 }
 
-class BallsApp extends StatelessWidget {
-  const BallsApp({super.key});
+class NotMyBubbles extends StatelessWidget {
+  NotMyBubbles({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(390, 844),
-      minTextAdapt: true,
-      builder: (context, snapshot) {
-        return ChangeNotifierProvider(
-          create: (context) => BubbleModelNotifier(),
-          child: MaterialApp(
-            home: Stack(
-              children: [
-                for (var index = 0; index < 69; index++)
-                  Balls(
-                    color: Color.fromRGBO(Random().nextInt(255),
-                        Random().nextInt(255), Random().nextInt(255), 0.8),
+        designSize: const Size(390, 844),
+        minTextAdapt: true,
+        builder: (context, snapshot) {
+          final maxHeight = (1.sh - 0.4.sw).round();
+          final maxWidth = (1.sw - 0.3.sw).round();
+          return MaterialApp(
+            home: ChangeNotifierProvider(
+              create: (exemplar) => RoundModelEditorNotifier(),
+              child: ColoredBox(
+                color: Colors.black87,
+                child: SafeArea(
+                  child: Stack(
+                    children: [
+                      for (var index = 0; index < 69; index++)
+                        Positioned(
+                          top: 5 + Random().nextInt(maxHeight).toDouble(),
+                          left: 0 + Random().nextInt(maxWidth).toDouble(),
+                          child: RoundWithActionWidget(
+                            color: Color.fromRGBO(
+                                Random().nextInt(255),
+                                Random().nextInt(255),
+                                Random().nextInt(255),
+                                0.9),
+                            size: 0.1 +
+                                Random().nextInt(30).toDouble() / 100 * 1.sw,
+                            removeBubble: false,
+                          ),
+                        ),
+                    ],
                   ),
-              ],
+                ),
+              ),
             ),
-          ),
-        );
-      },
-    );
+          );
+        });
+  }
+}
+
+class RoundModelEditorNotifier extends ChangeNotifier {
+  bool downsize = false;
+
+  void editSize({required bool resize}) {
+    downsize = false;
+    if (resize) {
+      downsize = true;
+      notifyListeners();
+    }
   }
 }
